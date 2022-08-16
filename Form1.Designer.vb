f1b1e33a7ac1eac5eceaeeca9c1d862509d22491
Pany-E <panyazae@gmail.com>
2022-08-16 14:06:20 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnOutput = New System.Windows.Forms.Button()
+        Me.btnOutputAll = New System.Windows.Forms.Button()
+        Me.BtnAddAll = New System.Windows.Forms.Button()
+        Me.BtnAverage = New System.Windows.Forms.Button()
+        Me.BtnAddOver20 = New System.Windows.Forms.Button()
+        Me.BtnMax = New System.Windows.Forms.Button()
+        Me.BtnMin = New System.Windows.Forms.Button()
+        Me.BtnDoubleAndOutput = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnOutput
+        '
+        Me.BtnOutput.Location = New System.Drawing.Point(-4, 64)
+        Me.BtnOutput.Name = "BtnOutput"
+        Me.BtnOutput.Size = New System.Drawing.Size(151, 39)
+        Me.BtnOutput.TabIndex = 0
+        Me.BtnOutput.Text = "Exercise1"
+        Me.BtnOutput.UseVisualStyleBackColor = True
+        '
+        'btnOutputAll
+        '
+        Me.btnOutputAll.Location = New System.Drawing.Point(-4, 127)
+        Me.btnOutputAll.Name = "btnOutputAll"
+        Me.btnOutputAll.Size = New System.Drawing.Size(151, 33)
+        Me.btnOutputAll.TabIndex = 1
+        Me.btnOutputAll.Text = "Exercise2"
+        Me.btnOutputAll.UseVisualStyleBackColor = True
+        '
+        'BtnAddAll
+        '
+        Me.BtnAddAll.Location = New System.Drawing.Point(-4, 177)
+        Me.BtnAddAll.Name = "BtnAddAll"
+        Me.BtnAddAll.Size = New System.Drawing.Size(151, 24)
+        Me.BtnAddAll.TabIndex = 2
+        Me.BtnAddAll.Text = "Exercise3"
+        Me.BtnAddAll.UseVisualStyleBackColor = True
+        '
+        'BtnAverage
+        '
+        Me.BtnAverage.Location = New System.Drawing.Point(-4, 221)
+        Me.BtnAverage.Name = "BtnAverage"
+        Me.BtnAverage.Size = New System.Drawing.Size(151, 27)
+        Me.BtnAverage.TabIndex = 3
+        Me.BtnAverage.Text = "Exercise4"
+        Me.BtnAverage.UseVisualStyleBackColor = True
+        '
+        'BtnAddOver20
+        '
+        Me.BtnAddOver20.Location = New System.Drawing.Point(-4, 263)
+        Me.BtnAddOver20.Name = "BtnAddOver20"
+        Me.BtnAddOver20.Size = New System.Drawing.Size(151, 28)
+        Me.BtnAddOver20.TabIndex = 4
+        Me.BtnAddOver20.Text = "Exercise5"
+        Me.BtnAddOver20.UseVisualStyleBackColor = True
+        '
+        'BtnMax
+        '
+        Me.BtnMax.Location = New System.Drawing.Point(-4, 309)
+        Me.BtnMax.Name = "BtnMax"
+        Me.BtnMax.Size = New System.Drawing.Size(151, 29)
+        Me.BtnMax.TabIndex = 5
+        Me.BtnMax.Text = "Exercise6"
+        Me.BtnMax.UseVisualStyleBackColor = True
+        '
+        'BtnMin
+        '
+        Me.BtnMin.Location = New System.Drawing.Point(-4, 355)
+        Me.BtnMin.Name = "BtnMin"
+        Me.BtnMin.Size = New System.Drawing.Size(151, 33)
+        Me.BtnMin.TabIndex = 6
+        Me.BtnMin.Text = "Exercise7"
+        Me.BtnMin.UseVisualStyleBackColor = True
+        '
+        'BtnDoubleAndOutput
+        '
+        Me.BtnDoubleAndOutput.Location = New System.Drawing.Point(-4, 406)
+        Me.BtnDoubleAndOutput.Name = "BtnDoubleAndOutput"
+        Me.BtnDoubleAndOutput.Size = New System.Drawing.Size(151, 32)
+        Me.BtnDoubleAndOutput.TabIndex = 7
+        Me.BtnDoubleAndOutput.Text = "Exercise8"
+        Me.BtnDoubleAndOutput.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnDoubleAndOutput)
+        Me.Controls.Add(Me.BtnMin)
+        Me.Controls.Add(Me.BtnMax)
+        Me.Controls.Add(Me.BtnAddOver20)
+        Me.Controls.Add(Me.BtnAverage)
+        Me.Controls.Add(Me.BtnAddAll)
+        Me.Controls.Add(Me.btnOutputAll)
+        Me.Controls.Add(Me.BtnOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnOutput As Button
+    Friend WithEvents btnOutputAll As Button
+    Friend WithEvents BtnAddAll As Button
+    Friend WithEvents BtnAverage As Button
+    Friend WithEvents BtnAddOver20 As Button
+    Friend WithEvents BtnMax As Button
+    Friend WithEvents BtnMin As Button
+    Friend WithEvents BtnDoubleAndOutput As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,164 @@
+﻿Public Class Form1
+    Private Sub BtnOutput_Click(sender As Object, e As EventArgs) Handles BtnOutput.Click
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            MsgBox(aiData(i))
+        Next
+
+    End Sub
+
+    Private Sub btnOutputAll_Click(sender As Object, e As EventArgs) Handles btnOutputAll.Click
+
+
+        Dim aiData(4) As Integer
+        Dim stConcat As String
+        stConcat = ""
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+        For i = 0 To 4
+            stConcat = stConcat & aiData(i) & vbNewLine
+
+        Next
+
+        MsgBox(stConcat)
+
+    End Sub
+
+    Private Sub BtnAddAll_Click(sender As Object, e As EventArgs) Handles BtnAddAll.Click
+
+        Dim aiData(4) As Integer
+        Dim holder As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            holder += aiData(i)
+
+        Next
+        MsgBox("The sum total is " & holder)
+
+    End Sub
+
+    Private Sub BtnAverage_Click(sender As Object, e As EventArgs) Handles BtnAverage.Click
+
+        Dim aiData(4) As Integer
+        Dim holder As Integer
+        Dim average As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            holder += aiData(i)
+        Next
+        average = holder / aiData.Length
+
+        MsgBox("The average is " & average)
+    End Sub
+
+    Private Sub BtnAddOver20_Click(sender As Object, e As EventArgs) Handles BtnAddOver20.Click
+
+        Dim aiData(4) As Integer
+        Dim sum As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            If aiData(i) > 20 Then
+                sum += aiData(i)
+            End If
+        Next
+        MsgBox("The sum of numbers over 20 is " & sum)
+
+    End Sub
+
+    Private Sub BtnMax_Click(sender As Object, e As EventArgs) Handles BtnMax.Click
+
+        Dim aiData(4) As Integer
+        Dim intLarge As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            If aiData(i) > intLarge Then
+                intLarge = aiData(i)
+            End If
+        Next
+
+        MsgBox("The largest number is " & intLarge)
+
+
+    End Sub
+
+    Private Sub BtnMin_Click(sender As Object, e As EventArgs) Handles BtnMin.Click
+
+
+        Dim aiData(4) As Integer
+        Dim intSmall As Integer
+        intSmall = 50
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            If aiData(i) < intSmall Then
+                intSmall = aiData(i)
+            End If
+        Next
+
+        MsgBox("The smallest number is " & intSmall)
+
+    End Sub
+
+    Private Sub BtnDoubleAndOutput_Click(sender As Object, e As EventArgs) Handles BtnDoubleAndOutput.Click
+
+        Dim aiData(4) As Integer
+        Dim aiDouble(4) As Integer
+        Dim stHolder As String
+        stHolder = ""
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            aiDouble(i) = 2 * aiData(i)
+            stHolder = stHolder & aiDouble(i) & vbNewLine
+        Next
+        MsgBox(stHolder)
+    End Sub
+End Class
